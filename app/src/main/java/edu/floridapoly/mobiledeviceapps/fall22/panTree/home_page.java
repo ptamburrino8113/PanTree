88e2a261e9b64ced41f5fb195873a7eedd84f55c
NoahBurton <cocacolakitten@hotmail.com>
2022-11-10 15:42:12 -0500

    Added list view to home with ability to add items to list

--- app/src/main/java/edu/floridapoly/mobiledeviceapps/fall22/panTree/home_page.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/fall22/panTree/home_page.java
@@ -5,15 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.content.Intent;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class home_page extends AppCompatActivity {
 
+    ListView listView;
+    ArrayList<String> list;
+    Button AddItem;
+    EditText ItemName;
+    ArrayAdapter<String> arrayAdapter;
     Button homeButton;
     Button sharesButton;
     Button logoutButton;
@@ -26,17 +37,35 @@ public class home_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        listView = (ListView) findViewById(R.id.listview);
+        AddItem = (Button) findViewById(R.id.AddItem);
+        ItemName = (EditText) findViewById(R.id.ItemName);
+        list = new ArrayList<String>();
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(arrayAdapter);
+
+        AddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String items = ItemName.getText().toString();
+
+                list.add(items);
+                listView.setAdapter(arrayAdapter);
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
         Bundle extras = getIntent().getExtras();
         mAuth = FirebaseAuth.getInstance();
-        if (extras != null)
-        {
+        if (extras != null) {
             String emailtext = extras.getString("email");
             email = emailtext;
         }
 
 
         homeButton = findViewById(R.id.homeButton);
-        homeButton.setOnClickListener(new View.OnClickListener(){
+        homeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Toast toast = Toast.makeText(getApplicationContext(),
                         "Home page where you can view your pantry items. A quantity will later be added to each",
@@ -46,7 +75,7 @@ public class home_page extends AppCompatActivity {
         });
 
         sharesButton = findViewById(R.id.sharesButton);
-        sharesButton.setOnClickListener(new View.OnClickListener(){
+        sharesButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(home_page.this, sharesActivity.class);
                 startActivity(intent);
@@ -54,7 +83,7 @@ public class home_page extends AppCompatActivity {
         });
 
         logoutButton = findViewById(R.id.logoutButton);
-        logoutButton.setOnClickListener(new View.OnClickListener(){
+        logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(home_page.this, MainActivity.class);
@@ -62,12 +91,13 @@ public class home_page extends AppCompatActivity {
             }
         });
     }
+
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
+        if (currentUser != null) {
             currentUser.reload();
         }
     }
